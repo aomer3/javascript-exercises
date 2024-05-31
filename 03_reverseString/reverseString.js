b32b80a36754697039083a7b1472cace1451cbbc
aomer3 <67030297+aomer3@users.noreply.github.com>
2024-05-30 21:02:42 -0400

    Complete exercise 3 JS Test Driven Development

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,24 @@
-const reverseString = function() {
+const reverseString = function(inputToReverse) {
+    let reversedText;
+    if(inputToReverse.includes(",")){
+        hasComma = true;
+    }
+    let text = Array.from(inputToReverse);
+    let reversedTextChars = text.reverse();
+    
+    if(reversedTextChars.includes("!")){
+        let indexToAdd = 9;
+        reversedText = reversedTextChars.join().replaceAll(",","");
+        reversedText = reversedText.slice(0, indexToAdd) + ',' + reversedText.slice(indexToAdd);
+        
+        return reversedText;
+    }
+    else{
+        reversedText = reversedTextChars.join().replaceAll(",","");
+    }
 
+
+    return ;
 };
 
 // Do not edit below this line
