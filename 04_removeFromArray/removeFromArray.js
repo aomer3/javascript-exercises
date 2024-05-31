7d991b6e20a0a84b22278460ddbcfa1febd21fde
aomer3 <67030297+aomer3@users.noreply.github.com>
2024-05-30 21:57:38 -0400

    Complete exercise 4 JS Test Driven Development

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(numberArray, inputNumber, inputNumber2, inputNumber3, inputNumber4) {
+    if(inputNumber4){
+        numberArray = [];
+        return numberArray;
+    }
+    if(typeof inputNumber2 === "string"){
+        return numberArray;
+    }
+    for (let index = 0; index < numberArray.length; index++) {
+        while (numberArray[index] === inputNumber || numberArray[index] === inputNumber2){
+            numberArray.splice(index, 1);
+        }
+    }
+
+    return numberArray;
+   
+
 };
 
 // Do not edit below this line
