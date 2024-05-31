c10a0b837f4f88cbdfae70ebb35b379f429fce48
aomer3 <67030297+aomer3@users.noreply.github.com>
2024-05-30 20:07:37 -0400

    Complete exercise 2 JS Test Driven Development

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word, repeats) {
+    let result = "";
 
+    if(repeats < 0){
+        return "ERROR";
+    }
+
+    for (let index = 0; index < repeats; index++) {
+        result += word;
+    }
+
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
