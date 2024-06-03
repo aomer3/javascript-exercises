30d9d800e90e78624d8b308a3ba926bb7897fbc5
aomer3 <67030297+aomer3@users.noreply.github.com>
2024-06-03 19:49:57 -0400

    Complete exercise 6 JS Test Driven Development

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+    let isLeapYear = false;
 
+    if(inputYear % 400 == 0){
+        isLeapYear = true;
+        return isLeapYear; 
+    }
+    else if (inputYear % 100 == 0 ){
+        return isLeapYear;
+    }
+    else if (inputYear % 4 == 0){
+        isLeapYear = true;
+        return isLeapYear;
+    }
+    else {
+        return isLeapYear;
+    }
 };
 
 // Do not edit below this line
