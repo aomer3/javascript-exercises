e1166433d6e7306320e43e3fd32a9d99d9e0c40d
aomer3 <67030297+aomer3@users.noreply.github.com>
2024-06-03 19:40:29 -0400

    Complete exercise 5 JS Test Driven Development

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let sum = 0;
 
+    if(firstNumber < 0 || secondNumber < 0){
+        return 'ERROR';
+    }
+
+    if((typeof firstNumber != 'number') || (typeof secondNumber != 'number')){
+        return 'ERROR';
+    }
+
+    if(firstNumber > secondNumber){
+        for (let index = firstNumber; index >= secondNumber; index--){
+            sum += index;
+            }
+
+    }
+    else{
+        for (let index = firstNumber; index <= secondNumber; index++){
+            sum += index;
+        }
+
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
