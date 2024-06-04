058d8e8d8d3f58387fe8c5f3f5dda2100132650c
aomer3 <67030297+aomer3@users.noreply.github.com>
2024-06-03 20:42:50 -0400

    Complete exercise 7 JS Test Driven Development

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemp) {
+  inputTemp = (inputTemp - 32) * (5/9);
+  return Number(inputTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTemp) {
+  inputTemp = ((9/5) * inputTemp) + 32;
+  return Number(inputTemp.toFixed(1));
 };
 
 // Do not edit below this line
