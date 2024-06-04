8fff305cd82930496905c9df471d381955b37381
aomer3 <67030297+aomer3@users.noreply.github.com>
2024-06-03 22:07:14 -0400

    Complete exercise 8 JS Test Driven Development

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(inputOne, inputTwo) {
+	return inputOne + inputTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(inputOne, inputTwo) {
+	return inputOne - inputTwo;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+
+  for (let index = 0; index < numbers.length; index++) {
+    sum += numbers[index];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  let sum = 1;
+  
+  for (let index = 0; index < numbers.length; index++) {
+    sum *= numbers[index];
+  }
 
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(nputOne, inputTwo) {
+  return nputOne ** inputTwo;
 };
 
-const factorial = function() {
-	
+const factorial = function(inputNumber) {
+  let sum = 1;
+  
+  for (let index = inputNumber; index > 0; index--) {
+    sum *= index;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
